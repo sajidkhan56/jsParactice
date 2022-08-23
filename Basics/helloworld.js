b487746c280d0ec1d5062f794ec38b8adab41a25
sajidkhan56 <sajid.web56@gmail.com>
2022-08-23 10:58:39 +0500

    add files

---
+++ Basics/helloworld.js
@@ -0,0 +1,6 @@
+document.getElementById('output').innerHTML = "hello world";
+window.alert("hello world");
+console.log("hello world");
+alert("hello world");
+document.write("hello world");
+print("hello world");
